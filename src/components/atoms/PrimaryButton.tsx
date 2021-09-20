cedafa450b90ee8037285432a440d98b652096ac
Atsu77 <kamnknon@gmail.com>
2021-09-20 12:17:30 +0900

    ログインコンポーネントを編集

--- src/components/atoms/PrimaryButton.tsx
+++ src/components/atoms/PrimaryButton.tsx
@@ -1,15 +1,25 @@
-import { Button } from "@chakra-ui/button";
 import React, { ReactNode, VFC } from "react";
+import { Button } from "@chakra-ui/button";
 
 type Props = {
   children: ReactNode;
+  onClick: () => void;
+  disabled?: boolean;
+  loading?: boolean;
 };
 
 export const PrimaryButton: VFC<Props> = (props) => {
-  const { children } = props;
+  const { children, onClick, disabled = false, loading = false } = props;
 
   return (
-    <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+    <Button
+      bg="teal.400"
+      color="white"
+      _hover={{ opacity: 0.8 }}
+      onClick={onClick}
+      disabled={disabled || loading}
+      isLoading={loading}
+    >
       {children}
     </Button>
   );
